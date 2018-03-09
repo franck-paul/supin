e36caaa986172065d4f193b3caa29905a0a78a80
franck <carnet.franck.paul@gmail.com>
2018-03-09 13:09:59 +0100

    Various code formatting, no more dublin core

--- _define.php
+++ _define.php
@@ -1,23 +1,22 @@
 <?php
-# -- BEGIN LICENSE BLOCK ----------------------------------
-# This file is part of Supin, a theme for Dotclear 2.
-#
-# Copyright (c) Franck Paul and contributors
-# carnet.franck.paul@gmail.com
-#
-# Licensed under the GPL version 2.0 license.
-# A copy of this license is available in LICENSE file or at
-# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-# -- END LICENSE BLOCK ------------------------------------
+/**
+ * @brief supin, a theme for Dotclear 2
+ *
+ * @package Dotclear
+ * @subpackage Themes
+ *
+ * @copyright Olivier Meunier & Association Dotclear
+ * @copyright GPL-2.0-only
+ */
 
-if (!defined('DC_RC_PATH')) { return; }
+if (!defined('DC_RC_PATH')) {return;}
 
 $this->registerModule(
-	/* Name */			"Supin",
-	/* Description*/	"Supin based on Aorakit 1B by Kozlika",
-	/* Author */		"Franck Paul",
-	/* Version */		'1.2',
-	array(
-		/* Type */		'type' =>			'theme'
-	)
+    "Supin",                                // Name
+    "Supin based on Aorakit 1B by Kozlika", // Description
+    "Franck Paul",                          // Author
+    '1.3',                                  // Version
+    array(
+        'type' => 'theme' // Type
+    )
 );
--- _public.php
+++ _public.php
@@ -1,81 +1,80 @@
 <?php
-# -- BEGIN LICENSE BLOCK ----------------------------------
-# This file is part of Supin, a theme for Dotclear 2.
-#
-# Copyright (c) Franck Paul and contributors
-# carnet.franck.paul@gmail.com
-#
-# Licensed under the GPL version 2.0 license.
-# A copy of this license is available in LICENSE file or at
-# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-# -- END LICENSE BLOCK ------------------------------------
+/**
+ * @brief supin, a theme for Dotclear 2
+ *
+ * @package Dotclear
+ * @subpackage Themes
+ *
+ * @copyright Olivier Meunier & Association Dotclear
+ * @copyright GPL-2.0-only
+ */
 
-if (!defined('DC_RC_PATH')) { return; }
+namespace themes\supin;
 
-$core->addBehavior('publicPrepend',array('behaviorSupinTheme','publicPrepend'));
-$core->addBehavior('templateBeforeBlock',array('behaviorSupinTheme','templateBeforeBlock'));
+if (!defined('DC_RC_PATH')) {return;}
 
-$core->tpl->addValue('Else',array('templateSupinTheme','templateElse'));
+$core->addBehavior('publicPrepend', array(__NAMESPACE__ . '\behaviorSupinTheme', 'publicPrepend'));
+$core->addBehavior('templateBeforeBlock', array(__NAMESPACE__ . '\behaviorSupinTheme', 'templateBeforeBlock'));
+
+$core->tpl->addValue('Else', array(__NAMESPACE__ . '\templateSupinTheme', 'templateElse'));
 
 class behaviorSupinTheme
 {
-	public static function publicPrepend($core)
-	{
-		$core->themes->loadModuleL10N($GLOBALS['__theme'],$GLOBALS['_lang'],'main');
-	}
+    public static function publicPrepend($core)
+    {
+        $core->themes->loadModuleL10N($GLOBALS['__theme'], $GLOBALS['_lang'], 'main');
+    }
 
-	public static function templateBeforeBlock($core,$b,$attr)
-	{
-	       if ($b == 'Entries' && isset($attr['exclude_current']) && $attr['exclude_current'] == 1)
-	       {
-		       return
-		       "<?php\n".
-		       '$params["sql"] .= "AND P.post_url != \'".$_ctx->posts->post_url."\' ";'."\n".
-		       "?>\n";
-	       }
-	}
+    public static function templateBeforeBlock($core, $b, $attr)
+    {
+        if ($b == 'Entries' && isset($attr['exclude_current']) && $attr['exclude_current'] == 1) {
+            return
+                "<?php\n" .
+                '$params["sql"] .= "AND P.post_url != \'".$_ctx->posts->post_url."\' ";' . "\n" .
+                "?>\n";
+        }
+    }
 }
 
 class templateSupinTheme
 {
-	static public function templateElse($attr)
-	{
-		return '<?php else: ?>';
-	}
+    public static function templateElse($attr)
+    {
+        return '<?php else: ?>';
+    }
 }
 
 class contextNav
 {
-	public static function getNextPosts($post_id,$ts,$dir,$nb,$nav,$navParam)
-	{
-		global $_ctx;
+    public static function getNextPosts($post_id, $ts, $dir, $nb, $nav, $navParam)
+    {
+        global $_ctx;
 
-		if ($dir > 0) {
-			$sign = '>';
-			$order = 'ASC';
-		}
-		else {
-			$sign = '<';
-			$order = 'DESC';
-		}
+        if ($dir > 0) {
+            $sign  = '>';
+            $order = 'ASC';
+        } else {
+            $sign  = '<';
+            $order = 'DESC';
+        }
 
-		$dt = date('YmdHis', (integer) $ts);
+        $dt = date('YmdHis', (integer) $ts);
 
-		// Récupération catégorie du billet courant (ctx)
-		$params['cat_url'] = $_ctx->posts->cat_url;
+        // Récupération catégorie du billet courant (ctx)
+        $params['cat_url'] = $_ctx->posts->cat_url;
 
-		$params['limit'] = $nb;
-		$params['order'] = 'post_dt ' . $order;
-		$params['sql'] = 'AND ' .
-			$GLOBALS['core']->blog->con->concat($GLOBALS['core']->blog->con->dateFormat('post_dt', '%Y%m%d%H%M%S'), "'.'", 'P.post_id') .
-			" $sign '$dt.$post_id'";
+        $params['limit'] = $nb;
+        $params['order'] = 'post_dt ' . $order;
+        $params['sql']   = 'AND ' .
+        $GLOBALS['core']->blog->con->concat($GLOBALS['core']->blog->con->dateFormat('post_dt', '%Y%m%d%H%M%S'), "'.'", 'P.post_id') .
+            " $sign '$dt.$post_id'";
 
-		$rs = $GLOBALS['core']->blog->getPosts($params);
+        $rs = $GLOBALS['core']->blog->getPosts($params);
 
-		if ($rs->isEmpty()) {
-			$rs = null;
-		}
+        if ($rs->isEmpty()) {
+            $rs = null;
+        }
 
-		return $rs;
-	}
+        return $rs;
+    }
 }
