c3872b02e3cf98c4361d5f911ec12cc7681b888c
franck <contact@open-time.net>
2026-08-13 16:49:33 +0200

    Enforces some verification before registering module

--- _define.php
+++ _define.php
@@ -9,20 +9,24 @@
  * @copyright Franck Paul (contact@open-time.net)
  * @copyright GPL-2.0
  */
-$this->registerModule(
-    'Supin',
-    'Supin based on Aorakit 1B by Kozlika',
-    'Franck Paul',
-    '7.1',
-    [
-        'date'     => '2026-04-05T11:53:08+0200',
-        'requires' => [['core', '2.36']],
-        'type'     => 'theme',
-        'overload' => true,
+declare(strict_types=1);
 
-        'details'    => 'https://open-time.net/?q=supin',
-        'support'    => 'https://github.com/franck-paul/supin',
-        'repository' => 'https://raw.githubusercontent.com/franck-paul/supin/main/dcstore.xml',
-        'license'    => 'gpl2',
-    ]
-);
+if (isset($this) && is_object($this) && method_exists($this, 'registerModule') && isset($this->id) && is_string($this->id)) {
+    $this->registerModule(
+        'Supin',
+        'Supin based on Aorakit 1B by Kozlika',
+        'Franck Paul',
+        '7.1',
+        [
+            'date'     => '2026-04-05T11:53:08+0200',
+            'requires' => [['core', '2.36']],
+            'type'     => 'theme',
+            'overload' => true,
+
+            'details'    => 'https://open-time.net/?q=supin',
+            'support'    => 'https://github.com/franck-paul/supin',
+            'repository' => 'https://raw.githubusercontent.com/franck-paul/supin/main/dcstore.xml',
+            'license'    => 'gpl2',
+        ]
+    );
+}
